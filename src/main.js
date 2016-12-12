2d5b60d8d7d145be73717a7b1eb0dbd3715de060
Daniel Rotter <daniel.rotter@gmail.com>
2016-12-12 21:19:50 +0100

    split into model, view and intent

--- src/main.js
+++ src/main.js
@@ -2,31 +2,49 @@ import xs from 'xstream';
 import { run } from '@cycle/xstream-run';
 import { makeDOMDriver, input, div, label } from '@cycle/dom';
 
-function main(sources) {
-    const sliderValue1$ = sources.DOM.select('.slider-1').events('change')
-        .map(ev => ev.target.value)
-        .map(value => parseInt(value))
-        .startWith(50);
+function intent(domSource) {
+    return {
+        changeValue1$: domSource.select('.slider-1').events('change')
+            .map(ev => ev.target.value)
+            .map(value => parseInt(value)),
+        changeValue2$: domSource.select('.slider-2').events('change')
+            .map(ev => ev.target.value)
+            .map(value => parseInt(value)),
+    };
+}
 
-    const sliderValue2$ = sources.DOM.select('.slider-2').events('change')
-        .map(ev => ev.target.value)
-        .map(value => parseInt(value))
-        .startWith(50);
+function model(actions) {
+    const sliderValue1$ = actions.changeValue1$.startWith(50);
+    const sliderValue2$ = actions.changeValue2$.startWith(50);
+
+    const score$ = xs.combine(sliderValue1$, sliderValue2$).map(([value1, value2]) => value1 * value2);
+
+    return xs.combine(sliderValue1$, sliderValue2$, score$);
+}
+
+function view(state$) {
+    return state$.map(([value1, value2, score]) => 
+        div([
+            div([
+                input('.slider-1', {attrs: { type: 'range'}}),
+                label(value1)
+            ]),
+            div([
+                input('.slider-2', {attrs: { type: 'range'}}),
+                label(value2)
+            ]),
+            div('Sum: ' + (score))
+        ])
+    );
+}
+
+function main(sources) {
+    const actions = intent(sources.DOM);
+    const state$ = model(actions);
+    const view$ = view(state$);
 
     return {
-        DOM: xs.combine(sliderValue1$, sliderValue2$).map(([value1, value2]) => 
-            div([
-                div([
-                    input('.slider-1', {attrs: { type: 'range'}}),
-                    label(value1)
-                ]),
-                div([
-                    input('.slider-2', {attrs: { type: 'range'}}),
-                    label(value2)
-                ]),
-                div('Sum: ' + (value1 + value2))
-            ])
-        )
+        DOM: view$
     };
 }
 
