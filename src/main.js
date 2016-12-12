49368b4d67481336eb5994ae00e63e1efd0b3a3d
Daniel Rotter <daniel.rotter@gmail.com>
2016-12-12 21:08:15 +0100

    added two adding sliders

--- src/main.js
+++ src/main.js
@@ -3,15 +3,28 @@ import { run } from '@cycle/xstream-run';
 import { makeDOMDriver, input, div, label } from '@cycle/dom';
 
 function main(sources) {
-    const sliderValue$ = sources.DOM.select('.slider').events('change')
+    const sliderValue1$ = sources.DOM.select('.slider-1').events('change')
         .map(ev => ev.target.value)
+        .map(value => parseInt(value))
+        .startWith(50);
+
+    const sliderValue2$ = sources.DOM.select('.slider-2').events('change')
+        .map(ev => ev.target.value)
+        .map(value => parseInt(value))
         .startWith(50);
 
     return {
-        DOM: sliderValue$.map(value => 
+        DOM: xs.combine(sliderValue1$, sliderValue2$).map(([value1, value2]) => 
             div([
-                input('.slider', {attrs: { type: 'range'}}),
-                label(value)
+                div([
+                    input('.slider-1', {attrs: { type: 'range'}}),
+                    label(value1)
+                ]),
+                div([
+                    input('.slider-2', {attrs: { type: 'range'}}),
+                    label(value2)
+                ]),
+                div('Sum: ' + (value1 + value2))
             ])
         )
     };
